bb308e1d99bf023c3d13fcb22f437cd2f9d385be
Jimin Yu <jm951027@gmail.com>
2022-06-03 01:12:57 +0900

    리드미 추가

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let taskList = [];
 let mode = "all";
 let filterList = [];
 
+//추가 버튼을 클릭하거나 엔터를 눌렀을 경우 할일추가 가능
 addBtn.addEventListener("mousedown", addTask);
 taskInput.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
@@ -18,12 +19,13 @@ for (let i = 1; i < tabs.length; i++) {
   });
 }
 
+//할일을 추가 하는 함수
 function addTask() {
   if (taskInput.value == "") {
     alert("할일을 입력해주세요");
     return;
   }
-  
+
   let task = {
     id: randomIDGenerate(),
     taskContent: taskInput.value,
@@ -34,6 +36,7 @@ function addTask() {
   render();
 }
 
+//UI렌더 부분
 function render() {
   let list = [];
   if (mode == "all") {
@@ -66,6 +69,7 @@ function render() {
   document.getElementById("task-board").innerHTML = resultHTML;
 }
 
+//할일을 완료 했는지 안했는지 껐다켰다 할수있는 함수
 function toggleComplete(id) {
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id == id) {
@@ -73,19 +77,20 @@ function toggleComplete(id) {
       break;
     }
   }
-  render();
+  render(); //UI가 변경 될 수 있도록 render()함수 호출
 }
 
+//삭제하는 함수
 function deleteTask(id) {
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id == id) {
       taskList.splice(i, 1);
     }
   }
-
   render();
 }
 
+//모두,진행중, 완료 탭별로 볼수 있음
 function filter(event) {
   mode = event.target.id;
   filterList = [];
@@ -116,6 +121,7 @@ function filter(event) {
   }
 }
 
+//아이템에 아이디를 부여해 내가 선택한 아이템이 어떤것인지 알기 위한 함수
 function randomIDGenerate() {
   return "_" + Math.random().toString(36).substr(2, 9);
 }
